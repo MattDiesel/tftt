f500de8ccb233dc45150a837c19109f71694b5c2
Matt Diesel <M@ttDiesel.co.uk>
2017-01-26 23:31:03 +0000

    Added tests for cell ref locations

--- test/unittests/treecellref_ut.cc
+++ test/unittests/treecellref_ut.cc
@@ -30,3 +30,27 @@ TEST(TreeCellRefTest, isBoundaryTest) {
     ASSERT_EQ(norm.isReflectBoundary(), false);
 }
 
+
+TEST(TreeCellRefTest, locations) {
+    if (!tftt::gtree.root) {
+        tftt::init(4.0, 2.0);
+    }
+
+    // Find cell {2}
+    tftt::cell_t cl = tftt::find(2);
+
+    ASSERT_EQ(cl.origin(0), 0.0);
+    ASSERT_EQ(cl.origin(1), 1.0);
+    ASSERT_EQ(cl.centre(0), 1.0);
+    ASSERT_EQ(cl.centre(1), 1.5);
+    ASSERT_EQ(cl.size(0), 2.0);
+    ASSERT_EQ(cl.size(1), 1.0);
+    ASSERT_EQ(cl.vertex(0, 0), 0.0);
+    ASSERT_EQ(cl.vertex(0, 1), 1.0);
+    ASSERT_EQ(cl.vertex(1, 0), 2.0);
+    ASSERT_EQ(cl.vertex(1, 1), 1.0);
+    ASSERT_EQ(cl.vertex(2, 0), 0.0);
+    ASSERT_EQ(cl.vertex(2, 1), 2.0);
+    ASSERT_EQ(cl.vertex(3, 0), 2.0);
+    ASSERT_EQ(cl.vertex(3, 1), 2.0);
+}
