ea0eae7985683467ccf65b780d29f13166739053
Matt Diesel <M@ttDiesel.co.uk>
2017-02-09 23:03:00 +0000

    added unit test for interpolation

--- test/unittests/treecellref_ut.cc
+++ test/unittests/treecellref_ut.cc
@@ -1,5 +1,6 @@
 
 #include <gtest/gtest.h>
+#include <cmath>
 
 #include "tftt/tftt.h"
 #include "tftt/cellref.h"
@@ -76,4 +77,50 @@ TEST(TreeCellRefTest, dataAccess) {
 
     cl.facedata(0).poisCoef = 1337;
     ASSERT_EQ(cl.facedata(0).poisCoef, 1337);
+}
+
+TEST(TreeCellRefTest, interpChild) {
+
+    tftt::reset();
+    tftt::init(4.0, 2.0);
+
+    tftt::cell_t cl0 = tftt::find(0);
+    tftt::cell_t cl1 = tftt::find(1);
+    tftt::cell_t cl2 = tftt::find(2);
+    tftt::cell_t cl3 = tftt::find(3);
+
+    tftt::refine(cl0);
+
+    cl0.child(0)->P = 0.0;
+    cl0.child(1)->P = 0.1;
+    cl0.child(2)->P = 0.2;
+    cl0.child(3)->P = 0.3;
+
+    cl1->P = 1.0;
+    cl2->P = 2.0;
+    cl3->P = 3.0;
+
+    double c12 = tftt::interpChild(cl1, 2, 0, [](tftt::data_t& dt) {
+        return dt.P;
+    });
+
+    ASSERT_LE(std::abs(c12 - 1.1), 1e-4);
+
+
+    tftt::refine(cl3);
+
+    cl3.child(0)->P = 3.0;
+    cl3.child(1)->P = 3.1;
+    cl3.child(2)->P = 3.2;
+    cl3.child(3)->P = 3.3;
+
+    tftt::drawMatrix("interpChild.matrix.pgm", 800, 400, [](tftt::data_t& dt, int max) {
+        return (dt.P / 4.0)*max;
+    });
+
+    c12 = tftt::interpChild(cl1, 2, 0, [](tftt::data_t& dt) {
+        return dt.P;
+    });
+
+    ASSERT_LE(std::abs(c12 - 1.22222), 1e-4);
 }
