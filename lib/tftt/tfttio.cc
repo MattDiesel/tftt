76cc692c55af3b7ad94e25a3708117824c312606
Matt Diesel <M@ttDiesel.co.uk>
2017-02-17 03:38:34 +0000

    Added functions for saving/loading trees from disk

--- lib/tftt/tfttio.cc
+++ lib/tftt/tfttio.cc
@@ -3,9 +3,12 @@
 #include <fstream>
 #include <ostream>
 
+#include <iostream> // Todo: Remove
+
 #include "tftt.h"
 #include "tree.h"
 #include "gray.h"
+#include "formatstring.h"
 
 #include "tfttio.h"
 
@@ -20,15 +23,24 @@ void drawMesh(std::string fname) {
 
 void drawMesh(std::ostream& os) {
     for (auto& c : leaves) {
-        for (int v = 0; v < 1<<DIM; v++) {
-            os << c.vertex(utils::toGray(v), 0);
-            for (int d = 1; d < DIM; d++) {
-                os << " " << c.vertex(utils::toGray(v), d);
-            }
-            os << "\n";
+        drawCell(os, c);
+    }
+}
+
+void drawCell(std::ostream& os, cell_t const& c) {
+    for (int v = 0; v < 1<<DIM; v++) {
+        os << c.vertex(utils::toGray(v), 0);
+        for (int d = 1; d < DIM; d++) {
+            os << " " << c.vertex(utils::toGray(v), d);
         }
         os << "\n";
     }
+
+    os << c.vertex(0, 0);
+    for (int d = 1; d < DIM; d++) {
+        os << " " << c.vertex(0, d);
+    }
+    os << "\n\n";
 }
 
 
@@ -63,19 +75,7 @@ void drawBoundaries(std::string fname) {
 
 void drawBoundary(std::ostream& os, int b) {
     for (auto& c : boundaryCells(b)) {
-        for (int v = 0; v < 1<<DIM; v++) {
-            os << c.vertex(utils::toGray(v), 0);
-            for (int d = 1; d < DIM; d++) {
-                os << " " << c.vertex(utils::toGray(v), d);
-            }
-            os << "\n";
-        }
-
-        os << c.vertex(0, 0);
-        for (int d = 1; d < DIM; d++) {
-            os << " " << c.vertex(0, d);
-        }
-        os << "\n\n";
+        drawCell(os, c);
     }
 }
 
@@ -110,6 +110,143 @@ void drawMatrix(std::ostream& os, int imgW, int imgH, fnDataNorm dataNorm) {
         }
     }
 }
+
+
+void saveTree(std::string fname) {
+    std::ofstream ofs(fname, std::ios::binary);
+    saveTree(ofs);
+}
+
+template<typename T>
+void writeVal(std::ostream& os, T t) {
+    os.write(reinterpret_cast<const char*>(&t), sizeof(T));
+}
+
+// void writeDouble(std::ostream& os, double d) {
+//     os.write(reinterpret_cast<const char*>(&d), 8);
+// }
+
+// void writeIdent(std::ostream& os, ident_t idt) {
+//     os.write(reinterpret_cast<const char*>(idt), sizeof(ident_t));
+// }
+
+void writeCell(std::ostream& os, cell_t const& cl) {
+    // os.write(reinterpret_cast<const char*>(&cl.id().id), sizeof(ident_t));
+    // os.write(reinterpret_cast<const char*>(&cell->processor), 1);
+    // os.write(reinterpret_cast<const char*>(&cell->data), sizeof(data_t));
+
+    writeVal(os, cl.id().id);
+    writeVal(os, cl.rank());
+    writeVal(os, cl.data());
+}
+
+void writeHeader(std::ostream& os) {
+    char magic[6] = "LT";
+    magic[2] = 3;
+    magic[3] = 0;
+    magic[4] = DIM;
+    magic[5] = sizeof(ident_t);
+    os.write(magic, 6);
+
+    uint16_t i = sizeof(data_t);
+    writeVal(os, i);
+
+    writeVal<double>(os, 0.0); // Origin fixed at 0.0, 0.0
+    writeVal<double>(os, 0.0);
+
+    writeVal(os, gtree.size[0]);
+    writeVal(os, gtree.size[1]);
+}
+
+
+void saveTree(std::ostream& os) {
+    os.imbue(std::locale::classic());
+
+    writeHeader(os);
+
+    for (auto const& cl : curve) {
+        writeCell(os, cl);
+    }
+}
+
+
+void splitToDisk(std::string fnameFmt) {
+    int node = 0;
+    std::ofstream ofs(tftt::utils::formatString(fnameFmt, 0), std::ios::binary);
+    writeHeader(ofs);
+
+    for (auto const& cl : curve) {
+        if (cl.rank() != node) {
+            ofs.close();
+            node++;
+            ofs.open(tftt::utils::formatString(fnameFmt, node), std::ios::binary);
+            writeHeader(ofs);
+        }
+
+        writeCell(ofs, cl);
+    }
+}
+
+
+template<typename T>
+void readVal(std::istream& ist, T& ret) {
+    ist.read(reinterpret_cast<char*>(&ret), sizeof(T));
+}
+
+
+void loadTree(std::string fname) {
+    std::ifstream ifs(fname, std::ios::binary);
+
+    // Read and check header
+    char header[40];
+    ifs.read(reinterpret_cast<char*>(header), 40);
+
+    if (header[0] != 'L' || header[1] != 'T') {
+        throw std::invalid_argument(fname);
+        throw std::invalid_argument("Input file is not a valid ltree file.");
+    }
+
+    int dim = int(header[4]);
+    int idlen = int(header[5]);
+    uint16_t datalen = *reinterpret_cast<uint16_t*>(&header[6]);
+
+    // std::cout << "Dim = " << dim << "\n";
+    // std::cout << "IdLen = " << idlen << "\n";
+    // std::cout << "DataLen = " << datalen << "\n";
+
+    // Origin not used
+    // double x = *reinterpret_cast<double*>(&header[8]);
+    // double y = *reinterpret_cast<double*>(&header[16]);
+    double w = *reinterpret_cast<double*>(&header[24]);
+    double h = *reinterpret_cast<double*>(&header[32]);
+
+    if (header[2] != 3)
+        throw std::runtime_error("Major version of file format incompatible");
+    if (dim != DIM)
+        throw std::runtime_error("Dimension of file tree does not match structure.");
+    if (datalen != sizeof(data_t))
+        throw std::runtime_error("Data size in file does not match structure.");
+    if (idlen != sizeof(ident_t))
+        throw std::runtime_error("ID size in file does not match structure.");
+
+    init(w, h);
+
+    ident_t toInsert;
+    cell_t newcl;
+
+    while (!ifs.eof()) {
+        readVal(ifs, toInsert);
+
+        newcl = insert(toInsert);
+        if (!newcl.isValid()) {
+            throw std::runtime_error("Error loading tree.");
+        }
+
+        readVal(ifs, newcl.rank());
+        readVal(ifs, newcl.data());
+    }
+}
+
 
 
 } // namespace tftt
--- lib/tftt/tfttio.h
+++ lib/tftt/tfttio.h
@@ -11,8 +11,11 @@
 
 namespace tftt {
 
+void drawCell(std::ostream& os, cell_t const& c);
+
 void drawMesh(std::string fname);
 void drawMesh(std::ostream& os);
+
 
 void drawCurve(std::string fname);
 void drawCurve(std::ostream& os);
@@ -22,6 +25,15 @@ void drawBoundary(std::ostream& os, int b);
 
 void drawMatrix(std::string fname, int imgW, int imgH, fnDataNorm dataNorm);
 void drawMatrix(std::ostream& os, int imgW, int imgH, fnDataNorm dataNorm);
+
+
+void saveTree(std::string fname);
+void saveTree(std::ostream& os);
+
+void loadTree(std::string fname);
+
+
+void splitToDisk(std::string fnameFmt);
 
 
 } // namespace tftt
