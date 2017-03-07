96427ace410c8836722fbffd5ca08039abd57295
Matt Diesel <M@ttDiesel.co.uk>
2017-03-07 02:12:32 +0000

    Faces are maintained on refinement.

--- lib/tftt/treegroup.cc
+++ lib/tftt/treegroup.cc
@@ -37,6 +37,39 @@ TreeGroup::TreeGroup()
     orientation = 0;
     next = CellRef();
     prev = CellRef();
+
+    // Boundaries
+    // Internal
+    constexpr int pairs[4][4] = {
+        {0, 1, 1, 0},
+        {0, 2, 2, 3},
+        {1, 2, 3, 3},
+        {2, 1, 3, 0}
+    };
+
+    for (int i = 0; i < 4; i++) {
+        cells[pairs[i][0]].faces[pairs[i][1]] =
+        cells[pairs[i][2]].faces[pairs[i][3]] =
+            new TreeFace(CellRef(this, pairs[i][0]), CellRef(this, pairs[i][2]), pairs[i][1] >> 1);
+    }
+
+    // Add external faces
+    constexpr int facecells[4][2] = {
+        {0, 2},
+        {1, 3},
+        {0, 1},
+        {2, 3}
+    };
+
+    cell_t cl;
+    for (int n = 0; n < 2*DIM; n++) {
+        // Add faces
+        for (int i = 0; i < 2; i++) {
+            cl = CellRef(this, facecells[n][i]);
+            cells[facecells[n][i]].faces[n] = 
+                new TreeFace(cl, cl.neighbour(n), n >> 1);
+        }
+    }
 }
 
 TreeGroup::TreeGroup(int b)
@@ -126,9 +159,64 @@ TreeGroup::TreeGroup(CellRef p)
             gtree.lastActive = CellRef(this, hilbChild(orientation, (1<<DIM)-1));
         }
     }
+
+    // Update faces
+    // Add internal faces
+
+    // Todo: Generalise:
+    // for (int n = 0; n < 4*(DIM-1); n++) {
+    //     fc = new TreeFace();
+    // }
+
+    constexpr int pairs[4][4] = {
+        {0, 1, 1, 0},
+        {0, 2, 2, 3},
+        {1, 2, 3, 3},
+        {2, 1, 3, 0}
+    };
+
+    for (int i = 0; i < 4; i++) {
+        cells[pairs[i][0]].faces[pairs[i][1]] =
+        cells[pairs[i][2]].faces[pairs[i][3]] =
+            new TreeFace(CellRef(this, pairs[i][0]), CellRef(this, pairs[i][2]), pairs[i][1] >> 1);
+    }
+
+    // Add external faces
+    constexpr int facecells[4][2] = {
+        {0, 2},
+        {1, 3},
+        {0, 1},
+        {2, 3}
+    };
+
+    cell_t cl;
+    for (int n = 0; n < 2*DIM; n++) {
+        if (neighbours[n].level() <= p.level()) {
+            // Add faces
+            for (int i = 0; i < 2; i++) {
+                cl = CellRef(this, facecells[n][i]);
+                cells[facecells[n][i]].faces[n] = 
+                    new TreeFace(cl, cl.neighbour(n), n >> 1);
+            }
+        }
+        else {
+            // Re-use faces
+            for (int i = 0; i < 2; i++) {
+                cl = CellRef(this, facecells[n][i]);
+                cells[facecells[n][i]].faces[n] = cl.neighbour(n).face(n ^ 1).face;
+            }
+        }
+    }
 }
 
 TreeGroup::~TreeGroup() {
+    constexpr int facecells[4][2] = {
+        {0, 2},
+        {1, 3},
+        {0, 1},
+        {2, 3}
+    };
+
     if (!gtree.destroying && boundary == -1) {
         if (prev.isValid()) {
             if (prev.group != parent.group) {
@@ -157,6 +245,26 @@ TreeGroup::~TreeGroup() {
         else {
             gtree.last = parent;
         }
+
+        // Faces 
+        for (int n = 0; n < 2*DIM; n++) {
+            if (neighbours[n].level() < id.level()) {
+                for (int i = 0; i < 2; i++) {
+                    delete cells[facecells[n][i]].faces[n];
+                }
+            }
+        }
+    }
+    else {
+        // delete faces nicely?
+        // Should be ok to delete memory twice.
+        // Todo: Make more efficient.
+
+        // for (int n = 0; n < 2*DIM; n++) {
+        //     for (int i = 0; i < 2*DIM; i++) {
+        //         delete cells[n].faces[i];
+        //     }
+        // }
     }
 
     for (int ch = 0; ch < cells.size(); ch++) {
