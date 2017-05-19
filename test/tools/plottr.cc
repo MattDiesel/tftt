e6b29a64d2c9b87c9153286a543dc7429f0f4672
Matt Diesel <M@ttDiesel.co.uk>
2017-05-20 00:35:29 +0100

    Added extra tools to plottr

--- test/tools/plottr.cc
+++ test/tools/plottr.cc
@@ -3,19 +3,27 @@
 #include <iostream>
 #include <fstream>
 #include <stdexcept>
+#include <map>
 
 #include "tftt/tftt.h"
-#include "tftt/tree.h"
+#include "tftt/structure/tree.h"
+
+#include "util/formatstring.h"
 
 using namespace tftt;
 
 template<typename T>
-void readVal2(std::istream& ist, T& ret) {
+void readVal2(std::istream& ist, T& ret)
+{
     ist.read(reinterpret_cast<char*>(&ret), sizeof(T));
 }
 
 
-void printTree(std::string fname, int n) {
+void printTree(std::string fname)
+{
+    std::string tempPath = "./plottrtmp";
+    std::map<int, std::ofstream> ofss;
+
     std::ifstream ifs(fname, std::ios::binary);
 
     // Read and check header
@@ -44,7 +52,7 @@ void printTree(std::string fname, int n) {
         throw std::runtime_error("ID size in file does not match structure.");
 
     init(w, h);
-    gtree.rank = n;
+    gtree.rank = -1;
 
     ident_t toInsert;
     cell_t newcl;
@@ -57,35 +65,36 @@ void printTree(std::string fname, int n) {
             throw std::runtime_error("Error loading tree.");
         }
 
+        std::cout << newcl << " inserted. ";
+
         readVal2(ifs, newcl.rank());
+        std::cout << "Rank=" << int(newcl.rank()) << "\n";
+
         readVal2(ifs, newcl.data());
 
-        std::cout << newcl.centre(0);
-        for (int d = 1; d < DIM; d++) {
-            std::cout << " " << newcl.centre(d);
+        auto search = ofss.find(newcl.rank());
+
+        if (search == ofss.end()) {
+            ofss[newcl.rank()] = std::ofstream(::util::formatString("{0}/r{1}.dat", tempPath, (int)newcl.rank()));
+            search = ofss.find(newcl.rank());
         }
-        
-        std::cout << "\n";
+
+        drawCell(search->second, newcl);
     }
 }
 
 
-int main(int argc, char const *argv[])
+int main(int argc, char const* argv[])
 {
 
     if (argc < 2) {
-        std::cout << "Usage: plottr [file] [n]" << std::endl;
+        std::cout << "Usage: plottr [file]" << std::endl;
         return 1;
-    }
-
-    int n = -1;
-    if (argc > 2) {
-        n = argv[2][0] - '0';
     }
 
     tftt::options.two2oneFlag = 0;
 
-    printTree(argv[1], n);
+    printTree(argv[1]);
 
     return 0;
 }
