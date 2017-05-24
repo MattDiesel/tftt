06644c7a0636acd056336f25cae7e827d0bf08f0
Matt Diesel <M@ttDiesel.co.uk>
2017-05-24 03:10:33 +0100

    Documented main parallel routines.

--- lib/tftt/parallel.h
+++ lib/tftt/parallel.h
@@ -9,25 +9,17 @@
 #include "cellref.h"
 
 
-// namespace boost {
-// namespace mpi {
-
-// template<> struct is_mpi_datatype<rt_data>
-//     : public mpl::true_ { };
-
-// }
-// } // namespace boost::mpi
-
-
 namespace tftt {
 
 
+//! Raw data sent over MPI to represent a cell
 struct CellMin {
     ident_t id;
     node_t rank;
     data_t data;
 };
 
+//! Raw MPI data to signal a ghost rank change
 struct GhostRankChange {
     ident_t id;
     node_t newRank;
@@ -35,18 +27,29 @@ struct GhostRankChange {
 
 
 //! Distribute cells across processors
+//! \param n The number of nodes to distribute across
 void distribute(int n);
 
+
+//! Synchronises ghost data across the world
+//! \param world The communicator object for the set of nodes
+//! \remark Further documentation of the method can be found in the final report.
 void syncGhosts(boost::mpi::communicator world);
+
+//! Moves cells to the left/right nodes
+//! \param world The communicator object for the set of nodes
+//! \param left The number of cells to move to the left. If cells are being
+//!             received this should be negative.
+//! \param right The number of cells to move to the right. If cells are being
+//!             received this should be negative.
+//! \remark Further documentation of the method can be found in the final report.
+void moveCells(boost::mpi::communicator world, int left, int right);
 
 
 void adaptParSwBegin();
 void adaptParSwPropogateVector(CellRef cl, uint8_t ref[2*DIM], uint8_t hold[2*DIM]);
 void adaptParSwPropogateLevel(CellRef cl, int dir, int lvl, int offset=0);
 void adaptParSwSetRefine(CellRef cl);
-
-
-void moveCells(boost::mpi::communicator world, int left, int right);
 
 
 } // namespace tftt
