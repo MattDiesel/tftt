8e7b0e21ac57fa05ca7ca16b761a5a9a59473f81
Matt Diesel <M@ttDiesel.co.uk>
2017-05-24 01:33:56 +0100

    Added doc comments to tftt header

--- lib/tftt/tftt.h
+++ lib/tftt/tftt.h
@@ -2,10 +2,16 @@
 #ifndef TFTT_PUBLIC_H
 #define TFTT_PUBLIC_H
 
+
+namespace tftt {
+
+
 // Constants for children, vertices, neighbours
 
-// Neighbours. P = positive dir, N = negative
-enum {
+//! Neighbours. P = positive dir, N = negative
+//! Neighbours indices are made up of the direction (1 for positive) in the
+//! lsb, and the dimension number in higher bits.
+enum NEIGHBOUR_INDICES {
     NB_XN = 0,
     NB_XP,
     NB_YN,
@@ -14,21 +20,34 @@ enum {
     NB_ZP
 };
 
-// Vertices (2d)
-enum {
+
+//! Vertices (2d)
+//! Vertices are numbered in the same way children are numbered as seen by the
+//! centre of the cell - using one bit per dimension, with a 1 in that position
+//! representing the positive direction. Dimensions are ordered with the lowest
+//! ranked dimension in the lsb, so {x,y,z} coordinates would be stored ZYX.
+enum VERTEX_INDICES {
     VT_BL = 0,
     VT_BR = 1,
     VT_TL = 2,
     VT_TR = 3
 };
 
-// Children (2d)
-enum {
+
+//! Children (2d)
+//! Children indexing is uses one bit per dimension, with a 1 in that position
+//! representing the positive direction. Dimensions are ordered with the lowest
+//! ranked dimension in the lsb, so {x,y,z} coordinates would be stored ZYX.
+enum CHILDREN_INDICES {
     CH_BL = 0,
     CH_BR = 1,
     CH_TL = 2,
     CH_TR = 3
 };
+
+
+} // namespace tftt
+
 
 #include "config.h"
 #include "treeid.h"
@@ -57,11 +76,25 @@ namespace tftt {
 
 // Miscellaneous functions that are mostly deprecated
 
+//! Checks all the nearby cells satisfy the predicate
 bool checkAround(cell_t cl, int dist, fnCheckCell check);
+
+//! Finds any nearby cell that satisfies the predicate
 bool findAround(cell_t cl, int dist, fnCheckCell check);
 
+//! Interpolate a value on a cell face
+//! \deprecated Cells should instead set the poisson coefficients and cell
+//! references to acheive the effect.
 double interpFace(cell_t cl, int fc, fnData dt);
+
+//! Interpolates the value a child cell would have, if it existed.
+//! \deprecated Cells should instead set the poisson coefficients and cell
+//! references to acheive the effect.
 double interpChild(cell_t cl, int ch, int forNb, fnData dt);
+
+//! Interpolate the value at a vertex
+//! \deprecated Vertex storage is currently being rewritten to store data on the
+//! ALE grid, making this function redundant.
 double interpALEVertex(cell_t cl, int v, fnData dt);
 
 
