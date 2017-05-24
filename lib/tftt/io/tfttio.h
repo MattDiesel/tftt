2f27fe4ddce0122f53fabeabb574e2d00b58d30c
Matt Diesel <M@ttDiesel.co.uk>
2017-05-24 04:09:07 +0100

    Added briefs for tftt io

--- lib/tftt/io/tfttio.h
+++ lib/tftt/io/tfttio.h
@@ -13,41 +13,67 @@
 namespace tftt {
 
 
+//! Writes the coordinates of each cell vertex to a stream
+//! \details When opened in gnuplot, this will outline the cell.
 void drawCell(std::ostream& os, cell_t const& c);
 
+//! Draws the leaves in the tree to a file
 void drawMesh(std::string fname);
+
+//! \copydoc drawMesh()
 void drawMesh(std::ostream& os);
 
 
+//! Draws the leaves of the tree whose rank is the current node
 void drawPartialMesh(std::string fname);
+
+//! Draws the leaves of the tree within a range
 void drawPartialMesh(std::string fname, cell_t from, cell_t to);
+//! \copydoc drawPartialMesh(std::string, cell_t, cell_t)
 void drawPartialMesh(std::ostream& os, cell_t from, cell_t to);
 
 
+//! Draws the ghost cells.
 void drawGhosts(std::string fname, int n);
+//! \copydoc drawGhosts()
 void drawGhosts(std::ostream& os, int n);
 
+
+//! Draws the border cells
 void drawBorder(std::string fname, int n);
+//! \copydoc drawBorder()
 void drawBorder(std::ostream& os, int n);
 
 
+//! Draws the space filling curve of the tree
 void drawCurve(std::string fname);
+//! \copydoc drawCurve()
 void drawCurve(std::ostream& os);
 
+//! Draws the curve of the leaves in the tree whose rank is the current node
 void drawPartialCurve(std::string fname);
+//! \copydoc drawPartialCurve()
 void drawPartialCurve(std::ostream& os);
 
+//! Draws the boundary cells
 void drawBoundaries(std::string fname);
+//! Draws the boundary cells of a single boundary
 void drawBoundary(std::ostream& os, int b);
 
+//! Draws the normalised data to an image file
 void drawMatrix(std::string fname, int imgW, int imgH, fnDataNorm dataNorm);
+//! \copydoc drawMatrix
 void drawMatrix(std::ostream& os, int imgW, int imgH, fnDataNorm dataNorm);
 
+//! Plots the data
 void plotMatrix(std::string fname, fnData dt);
+//! \copydoc plotMatrix
 void plotMatrix(std::ostream& os, fnData dt);
 
 
+//! Draws the cells required to perform the poisson calculations
 void drawPoissonNeighbourhood(std::string fname, cell_t cl);
+//! \copydoc drawPoissonNeighbourhood
 void drawPoissonNeighbourhood(std::ostream& os, cell_t cl);
 
 
