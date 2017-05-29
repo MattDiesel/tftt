2cd2cfb0db352eaf2f596b25b51b4768aa87aaa3
Matt Diesel <M@ttDiesel.co.uk>
2017-05-29 01:57:09 +0100

    Added tikz io functions

---
+++ lib/tftt/io/tikz.cc
@@ -0,0 +1,95 @@
+
+#include <string>
+#include <ostream>
+#include <fstream>
+
+#include "util/formatstring.h"
+
+#include "../config.h"
+#include "../cellref.h"
+#include "../structure/tree.h"
+#include "../iter/all.h"
+
+#include "tikz.h"
+
+
+namespace tftt {
+namespace tikz {
+
+
+void meshLayer(std::ostream& os, cell_t cl, int layer)
+{
+    if (cl.level() == layer) {
+        os << "\\node at ("
+           << cl.centre(0) << "," << cl.centre(1) << ") {};\n";
+    }
+    else if (cl.hasChildren()) {
+        for (auto ch : *cl.children()) {
+            meshLayer(os, ch, layer);
+        }
+    }
+}
+
+
+void meshLayer(std::ostream& ofs, int layer)
+{
+    cell_t root = cell_t(-1);
+
+    double w = gtree.size[0] / (2 << layer);
+    double h = gtree.size[1] / (2 << layer);
+
+    ofs << "\\begin{scope}[minimum width=" << w
+        << "cm-\\the\\pgflinewidth,minimum height=" << h
+        << "cm-\\the\\pgflinewidth,inner sep=0,draw,shape=rectangle,anchor=center]\n";
+
+    meshLayer(ofs, root, layer);
+
+    ofs << "\\end{scope}\n";
+}
+
+
+void meshLayer(std::string fname, int layer)
+{
+    std::ofstream ofs(fname);
+    meshLayer(ofs, layer);
+}
+
+
+void mesh(std::string fname, int maxDepth)
+{
+    std::ofstream ofs(fname);
+
+    for (int d = 0; d < maxDepth; d++) {
+        meshLayer(ofs, d);
+    }
+}
+
+
+template<class T>
+void curve(std::string fname, T set)
+{
+    std::ofstream ofs(fname);
+
+    cell_t prev;
+    for (auto cl : set) {
+        if (!prev.isValid()) {
+            prev = cl;
+            continue;
+        }
+
+        ofs << "\\draw (" << prev.centre(0) << "," << prev.centre(1)
+            << ") -- (" << cl.centre(0) << "," << cl.centre(1) << ");\n";
+    }
+
+    ofs.close();
+}
+
+
+void hilbert(std::string fname)
+{
+    curve(fname, tftt::curve);
+}
+
+
+} // namespace tikz
+} // namespace tftt
---
+++ lib/tftt/io/tikz.h
@@ -0,0 +1,30 @@
+
+#ifndef TFTT_IO_TIKZ_H
+#define TFTT_IO_TIKZ_H
+
+
+#include <string>
+#include <ostream>
+
+#include "../config.h"
+#include "../cellref.h"
+
+
+namespace tftt {
+namespace tikz {
+
+
+void meshLayer(std::ostream& os, cell_t cl, int layer);
+void meshLayer(std::ostream& ofs, int layer);
+void meshLayer(std::string fname, int layer);
+
+void mesh(std::string fname, int maxDepth);
+
+void hilbert(std::string fname);
+
+
+} // namespace tikz
+} // namespace tftt
+
+
+#endif
