59b6f54ffbf9761d79955dd5a6fcd1dce8a9f537
Matt Diesel <M@ttDiesel.co.uk>
2017-05-30 17:06:15 +0100

    Removed const modifiers from headers

--- docs/report/headers/cellref.h
+++ docs/report/headers/cellref.h
@@ -3,28 +3,28 @@ private:
     TreeCell* _cell;
 public:
     // Basic Orthotree functions
-    data_t& data();                    // $\mathcal{C}\acc\textproc{data}$
-    CellRef parent() const;            // $\mathcal{C}\acc\textproc{parent}$
-    TreeGroup* children() const;       // $\mathcal{C}\acc\vect{\mathcal{K}}$
-    CellRef child(int c) const;        // $\mathcal{C}\acc\mathcal{K}_c$
+    data_t& data();              // $\mathcal{C}\acc\textproc{data}$
+    CellRef parent();            // $\mathcal{C}\acc\textproc{parent}$
+    TreeGroup* children();       // $\mathcal{C}\acc\vect{\mathcal{K}}$
+    CellRef child(int c);        // $\mathcal{C}\acc\mathcal{K}_c$
 
     // Core FTT functionality
-    CellRef neighbour(int n) const;    // $\mathcal{C}\acc\mathcal{N}_n$
+    CellRef neighbour(int n);    // $\mathcal{C}\acc\mathcal{N}_n$
 
     // TFTT functionality
-    int orientation() const;           // $\mathcal{C}\acc\textproc{orient}$
-    CellRef next() const;              // $\mathcal{C}\acc\textproc{next}$
-    CellRef prev() const;              // $\mathcal{C}\acc\textproc{prev}$
-    CellRef firstChild() const;         // $\mathcal{C}\acc\textproc{firstChild}$
-    CellRef lastChild() const;         // $\mathcal{C}\acc\textproc{lastChild}$
+    int& orientation();          // $\mathcal{C}\acc\textproc{orient}$
+    CellRef& next();             // $\mathcal{C}\acc\textproc{next}$
+    CellRef& prev();             // $\mathcal{C}\acc\textproc{prev}$
+    CellRef firstChild();         // $\mathcal{C}\acc\textproc{firstChild}$
+    CellRef lastChild();         // $\mathcal{C}\acc\textproc{lastChild}$
 
     // Parallel 
-    node_t& rank();                    // $\mathcal{C}\acc\textproc{rank}$
-    ident_t id() const;                // $\mathcal{C}\acc\textproc{ident}$
+    node_t& rank();              // $\mathcal{C}\acc\textproc{rank}$
+    ident_t id();                // $\mathcal{C}\acc\textproc{ident}$
 
     // Poisson Neighbourhoods
-    int poisNeighbours() const;        // $\abs{\mathcal{C}\acc\vect{\mathcal{P}}}$
-    double poisCoef(int p) const;      // $\mathcal{C}\acc\beta_p$
-    CellRef poisNeighb(int p) const;   // $\mathcal{C}\acc\mathcal{P}_p$
-    double poisCentralCoef() const;    // $\mathcal{C}\acc\alpha$
+    int poisNeighbours();        // $\abs{\mathcal{C}\acc\vect{\mathcal{P}}}$
+    double& poisCoef(int p);     // $\mathcal{C}\acc\beta_p$
+    CellRef& poisNeighb(int p);  // $\mathcal{C}\acc\mathcal{P}_p$
+    double& poisCentralCoef();   // $\mathcal{C}\acc\alpha$
 };
